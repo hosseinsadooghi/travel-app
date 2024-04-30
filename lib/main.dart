f1eb484eabc429ca98ce26afc88a9cc594227756
hosseinsadooghi <hosseon.sadoghi@gmail.com>
2024-04-30 14:29:47 +0330

    second mini project

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class _MyHomePageState extends State<MainPage> {
       body: SafeArea(
         child: Column(
           children: [
+            //head part
             Container(
               color: Colors.white,
               width: double.infinity,
@@ -62,7 +63,8 @@ class _MyHomePageState extends State<MainPage> {
                           image: DecorationImage(
                             image: AssetImage(travelList[_selectedIndex].image),
                             fit: BoxFit.cover,
-                          )),
+                          )
+                      ),
                     ),
                   ),
                   //head icons
@@ -78,8 +80,7 @@ class _MyHomePageState extends State<MainPage> {
                           InkWell(
                             onTap: () {
                               setState(() {
-                                travelList[_selectedIndex].liked =
-                                    !travelList[_selectedIndex].liked;
+                                travelList[_selectedIndex].liked = !travelList[_selectedIndex].liked;
                               });
                             },
                             child: AnimatedContainer(
@@ -169,185 +170,192 @@ class _MyHomePageState extends State<MainPage> {
                 ],
               ),
             ),
+            //details part
             Expanded(
-              child: Stack(alignment: Alignment.topCenter, children: [
-                Positioned(
-                  bottom: 10,
-                  child: Column(
-                    children: [
-                      //cards
-                      Container(
-                        width: size.width / 10 * 8,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            //distance
-                            Card(
-                                color: Colors.white,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20),
-                                    side: const BorderSide(
-                                        color: Color.fromARGB(40, 0, 0, 0),
-                                        width: 1)),
-                                child: Container(
-                                  height: 85,
-                                  width: 85,
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      const Text(
-                                        "Distance",
-                                        style: TextStyle(fontSize: 14),
+              child: Stack(
+                alignment: Alignment.topCenter, 
+                children: [
+                  Positioned(
+                    bottom: 10,
+                    child: Column(
+                      children: [
+                        //cards
+                        Container(
+                          width: size.width / 10 * 8,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              //distance
+                              Card(
+                                  color: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                      side: const BorderSide(
+                                          color: Color.fromARGB(40, 0, 0, 0),
+                                          width: 1)
                                       ),
-                                      Text(
-                                          "${travelList[_selectedIndex].distance} Km",
-                                          style: const TextStyle(
-                                              color: Color.fromARGB(
-                                                  255, 120, 202, 222),
-                                              fontSize: 20)),
-                                      const SizedBox()
-                                    ],
-                                  ),
-                                )),
-                            //Temp
-                            Card(
-                                color: Colors.white,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20),
-                                    side: const BorderSide(
-                                        color: Color.fromARGB(40, 0, 0, 0),
-                                        width: 1)),
-                                child: Container(
-                                  height: 85,
-                                  width: 85,
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      const Text(
-                                        "Temp",
-                                        style: TextStyle(fontSize: 14),
+                                  child: Container(
+                                    height: 85,
+                                    width: 85,
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        const Text(
+                                          "Distance",
+                                          style: TextStyle(fontSize: 14),
+                                        ),
+                                        Text(
+                                            "${travelList[_selectedIndex].distance} Km",
+                                            style: const TextStyle(
+                                                color: Color.fromARGB(
+                                                    255, 120, 202, 222),
+                                                fontSize: 20)
+                                            ),
+                                        const SizedBox()
+                                      ],
+                                    ),
+                                  )),
+                              //Temp
+                              Card(
+                                  color: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                      side: const BorderSide(
+                                          color: Color.fromARGB(40, 0, 0, 0),
+                                          width: 1)
                                       ),
-                                      Text(
-                                          "${travelList[_selectedIndex].temp}\u00b0 C",
-                                          style: const TextStyle(
-                                              color: Color.fromARGB(
-                                                  255, 120, 202, 222),
-                                              fontSize: 20)),
-                                      const SizedBox()
-                                    ],
-                                  ),
-                                )),
-                            //Rating
-                            Card(
-                                color: Colors.white,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20),
-                                    side: const BorderSide(
-                                        color: Color.fromARGB(40, 0, 0, 0),
-                                        width: 1)),
-                                child: Container(
-                                  height: 85,
-                                  width: 85,
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      const Text(
-                                        "Rating",
-                                        style: TextStyle(fontSize: 14),
-                                      ),
-                                      Text(travelList[_selectedIndex].rating,
-                                          style: const TextStyle(
-                                              color: Color.fromARGB(
-                                                  255, 120, 202, 222),
-                                              fontSize: 20)),
-                                      const SizedBox()
-                                    ],
-                                  ),
-                                )),
-                          ],
-                        ),
-                      ),
-                      //description title
-                      Container(
-                        width: size.width / 10 * 8,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const SizedBox(
-                              height: 8,
-                            ),
-                            const Text(
-                              "Description",
-                              style: TextStyle(fontSize: 16),
-                            ),
-                            const SizedBox(
-                              height: 8,
-                            )
-                          ],
-                        ),
-                      ),
-                      //description
-                      Container(
-                        height: size.height / 9,
-                        width: size.width / 10 * 8,
-                        child: SingleChildScrollView(
-                          child: ExpandableText(
-                            travelList[_selectedIndex].description,
-                            style: const TextStyle(fontSize: 12),
-                            expandText: "read more",
-                            collapseText: "read less",
-                            maxLines: 5,
-                            linkColor: Color.fromARGB(255, 120, 202, 222),
+                                  child: Container(
+                                    height: 85,
+                                    width: 85,
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        const Text(
+                                          "Temp",
+                                          style: TextStyle(fontSize: 14),
+                                        ),
+                                        Text(
+                                            "${travelList[_selectedIndex].temp}\u00b0 C",
+                                            style: const TextStyle(
+                                                color: Color.fromARGB(
+                                                    255, 120, 202, 222),
+                                                fontSize: 20)
+                                            ),
+                                        const SizedBox()
+                                      ],
+                                    ),
+                                  )),
+                              //Rating
+                              Card(
+                                  color: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                      side: const BorderSide(
+                                          color: Color.fromARGB(40, 0, 0, 0),
+                                          width: 1)),
+                                  child: Container(
+                                    height: 85,
+                                    width: 85,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        const Text(
+                                          "Rating",
+                                          style: TextStyle(fontSize: 14),
+                                        ),
+                                        Text(travelList[_selectedIndex].rating,
+                                            style: const TextStyle(
+                                                color: Color.fromARGB(
+                                                    255, 120, 202, 222),
+                                                fontSize: 20)),
+                                        const SizedBox()
+                                      ],
+                                    ),
+                                  )),
+                            ],
                           ),
                         ),
-                      ),
-                      //price and sell botton
-                      Container(
-                        width: size.width / 10 * 8,
-                        height: size.height / 10,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text(
-                                  "Total Price",
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.normal,
-                                      color: Colors.grey),
-                                ),
-                                Text(
-                                  "\$${travelList[_selectedIndex].price}",
-                                  style: const TextStyle(
-                                      fontSize: 30,
-                                      fontWeight: FontWeight.bold),
-                                )
-                              ],
-                            ),
-                            //button
-                            Container(
-                              width: 100,
-                              height: 100,
-                              decoration: const BoxDecoration(
-                                  shape: BoxShape.circle, color: Colors.black),
-                              child: const Icon(
-                                CupertinoIcons.greaterthan,
-                                color: Colors.white,
-                                size: 35,
+                        //description title
+                        Container(
+                          width: size.width / 10 * 8,
+                          child: const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: 8,
                               ),
-                            ),
-                          ],
+                              Text(
+                                "Description",
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              SizedBox(
+                                height: 8,
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-              ]),
+                        //description
+                        Container(
+                          height: size.height / 9,
+                          width: size.width / 10 * 8,
+                          child: SingleChildScrollView(
+                            child: ExpandableText(
+                              travelList[_selectedIndex].description,
+                              style: const TextStyle(fontSize: 12),
+                              expandText: "read more",
+                              collapseText: "read less",
+                              maxLines: 5,
+                              linkColor: const Color.fromARGB(255, 120, 202, 222),
+                            ),
+                          ),
+                        ),
+                        //price and sell botton
+                        Container(
+                          width: size.width / 10 * 8,
+                          height: size.height / 10,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              //price
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    "Total Price",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.normal,
+                                        color: Colors.grey),
+                                  ),
+                                  Text(
+                                    "\$${travelList[_selectedIndex].price}",
+                                    style: const TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                              //button
+                              Container(
+                                width: 100,
+                                height: 100,
+                                decoration: const BoxDecoration(
+                                    shape: BoxShape.circle, color: Colors.black),
+                                child: const Icon(
+                                  CupertinoIcons.greaterthan,
+                                  color: Colors.white,
+                                  size: 35,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),              
+                ]
+              ),
             ),
           ],
         ),
